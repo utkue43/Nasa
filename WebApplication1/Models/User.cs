2f342c66f84956d4c61e00f2ab9ee884da943e99
erkoc <erkoc@DESKTOP-LKTFLDM>
2023-10-07 12:06:27 +0300

    Proje dosyası ekle.

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
---
+++ WebApplication1/Repository/JWTManagerRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApplication1.Repository
+{
+    public class JWTManagerRepository
+    {
+    }
+}
