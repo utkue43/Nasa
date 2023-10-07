570ab6a1d7095ecf989743fe197328f87275f72d
Ömer Cesur <96197600+OmerCesur@users.noreply.github.com>
2023-10-07 17:51:04 +0300

    ömer

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -1,1 +1,21 @@
-﻿
+﻿function increase() {
+    var increases = document.querySelectorAll(".counter");
+
+    for (var i = 0; i < increases.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = increases[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+        console.log("kls");
+            for (var i = 0; i < 500; i++) {
+                document.getElementById("c1").innerHTML = i;
+                setTimeout(0.001);
+            }
+        } else {
+            
+        }
+    }
+}
+
+window.addEventListener("scroll", increase);
